6e0807e78162bf3a118a6424e0d2856ef46b82ee
natta1995 <natalie.hallerdal@yahoo.se>
2026-05-11 15:08:23 +0200

    Adding bulter page (for more game feeling)

---
+++ src/pages/ButlerStudyPage.jsx
@@ -0,0 +1,35 @@
+import {butlerMessages} from "../data/ButlerMessages";
+import backgroundImgButler from "../Img/StudyImgButler.png";
+import { useNavigate } from "react-router-dom";
+
+import { useEffect, useState } from "react";
+
+const  ButlerStudyPage = () => {
+  const [message, setMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * butlerMessages.length);
+    setMessage(butlerMessages[randomIndex]);
+
+    const timer = setTimeout(() => {
+      navigate("/study");
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+return (
+  <div
+  className="butler-view"
+  style={{ backgroundImage: `url(${backgroundImgButler})` }}
+>
+  <div className="butler-message">
+    {message}
+  </div>
+</div>
+);
+}
+
+export default ButlerStudyPage;
